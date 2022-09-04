22d95508b159c4b96176c0f5e1464b432644e9cd
misobae <misobae0731@gmail.com>
2022-09-04 21:05:11 +0900

    Feat: About 페이지 추가

--- vue3-movie-app/src/routes/About.vue
+++ vue3-movie-app/src/routes/About.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>About</h1>
+  <div class="about">
+    <div class="icon">
+      😊
+    </div>
+    <div class="name">
+      {{ name }}
+    </div>
+    <div>{{ email }}</div>
+    <div>{{ github }}</div>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    name() {
+      return this.$store.state.about.name
+    },
+    email() {
+      return this.$store.state.about.email
+    },
+    github() {
+      return this.$store.state.about.github
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.about {
+  text-align: center;
+  .icon {
+    font-size: 80px;
+    }
+  .name {
+    font-size: 40px;
+    font-family: "Oswald", sans-serif;
+    margin-bottom: 20px;
+  }
+}
+</style>
---
+++ vue3-movie-app/src/store/about.js
@@ -0,0 +1,8 @@
+export default {
+  namespaced: true,
+  state: () => ({
+    name: 'MISO',
+    email: 'misobae0731@gmail.com',
+    github: 'https://github.com/misobae'
+  })
+}
